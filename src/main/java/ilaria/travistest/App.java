10781aca01e412980162de89acdded74ad2f8e19
Ilaria Pigazzini <ipiga94@gmail.com>
2020-11-26 17:00:53 +0100

    Third push with travis

--- src/main/java/ilaria/travistest/App.java
+++ src/main/java/ilaria/travistest/App.java
@@ -11,7 +11,7 @@ public class App
     	if(args.length != 0) {
     		System.out.println( "Hello " + args[0] ); 		
     	} else {
-    		System.out.println( "OOkkeiii");
+    		System.out.println( "OOkkeiii2");
     		System.out.println( "I would love to see travis working...");
     		
     	}
